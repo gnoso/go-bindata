0ef77358a5930ec0cc84123a3187d2cedad3fe9f
Peter Waldschmidt <peterw@gnoso.com>
2012-12-20 23:42:47 -0500

    Hack the utility….make command-line simpler, and loop over an entire directory.

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path"
-	"runtime"
+	"path/filepath"
 	"strings"
 	"unicode"
 )
@@ -20,108 +19,77 @@ const (
 )
 
 var (
-	pipe         = false
-	in           = flag.String("i", "", "Path to the input file. Alternatively, pipe the file data into stdin.")
-	out          = flag.String("o", "", "Optional path to the output file.")
-	pkgname      = flag.String("p", "", "Optional name of the package to generate.")
-	funcname     = flag.String("f", "", "Optional name of the function/variable to generate.")
+	dir          = flag.String("dir", "", "Input directory. Processes all files in the path recursively.")
+	out          = flag.String("o", "", "Path to the output files.")
+	pkgname      = flag.String("p", "", "Name of the package to generate.")
 	uncompressed = flag.Bool("u", false, "The specified resource will /not/ be GZIP compressed when this flag is specified. This alters the generated output code.")
 	nomemcopy    = flag.Bool("m", false, "Use the memcopy hack to get rid of unnecessary memcopies. Refer to the documentation to see what implications this carries.")
-	version      = flag.Bool("v", false, "Display version information.")
 )
 
 func main() {
-	parseArgs()
+	flag.Parse()
 
-	if pipe {
-		translate(os.Stdin, os.Stdout, *pkgname, *funcname, *uncompressed, *nomemcopy)
-	} else {
-		fs, err := os.Open(*in)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "[e] %s\n", err)
-			return
-		}
+	err := filepath.Walk(*dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
 
-		defer fs.Close()
+			if !info.IsDir() && info.Name() != ".DS_Store" {
+				fmt.Fprintf(os.Stderr, "[i] translating file %s\n", path)
+				err = translate_file(path)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "[e] %s\n", err)
+					return err
+				}
+			}
+			return nil
+		})
 
-		fd, err := os.Create(*out)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "[e] %s\n", err)
-			return
-		}
-
-		defer fd.Close()
-
-		translate(fs, fd, *pkgname, *funcname, *uncompressed, *nomemcopy)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[e] %s\n", err)
+		return
 	}
 
 	fmt.Fprintln(os.Stdout, "[i] Done.")
 }
 
-// parseArgs processes and verifies commandline arguments.
-func parseArgs() {
-	flag.Parse()
+func translate_file(infile string) error {
 
-	if *version {
-		fmt.Fprintf(os.Stdout, "%s v%s (Go runtime %s)\n",
-			AppName, AppVersion, runtime.Version())
-		os.Exit(0)
+	funcName := genFunctionName(strings.Replace(infile, *dir+"/", "", -1))
+	outfile := *out + "/" + funcName + ".go"
+
+	fs, err := os.Open(infile)
+	if err != nil {
+		return err
 	}
 
-	pipe = len(*in) == 0
+	defer fs.Close()
 
-	if !pipe && len(*out) == 0 {
-		// Ensure we create our own output filename that does not already exist.
-		dir, file := path.Split(*in)
-
-		*out = path.Join(dir, file) + ".go"
-		if _, err := os.Lstat(*out); err == nil {
-			// File already exists. Pad name with a sequential number until we
-			// find a name that is available.
-			count := 0
-			for {
-				f := path.Join(dir, fmt.Sprintf("%s.%d.go", file, count))
-				if _, err := os.Lstat(f); err != nil {
-					*out = f
-					break
-				}
-
-				count++
-			}
-		}
-
-		fmt.Fprintf(os.Stderr, "[w] No output file specified. Using '%s'.\n", *out)
+	fd, err := os.Create(outfile)
+	if err != nil {
+		return err
 	}
 
-	if len(*pkgname) == 0 {
-		fmt.Fprintln(os.Stderr, "[w] No package name specified. Using 'main'.")
-		*pkgname = "main"
-	} else {
-		if unicode.IsDigit(rune((*pkgname)[0])) {
-			// Identifier can't start with a digit.
-			*pkgname = "_" + *pkgname
-		}
+	defer fd.Close()
+
+	translate(fs, fd, *pkgname, funcName, *uncompressed, *nomemcopy)
+	return nil
+}
+
+func genFunctionName(filename string) string {
+	funcName := filename
+	funcName = strings.ToLower(funcName)
+	funcName = strings.Replace(funcName, "/", "_", -1)
+	funcName = strings.Replace(funcName, " ", "_", -1)
+	funcName = strings.Replace(funcName, ".", "_", -1)
+	funcName = strings.Replace(funcName, "-", "_", -1)
+
+	if unicode.IsDigit(rune(funcName[0])) {
+		// Identifier can't start with a digit.
+		funcName = "_" + funcName
 	}
 
-	if len(*funcname) == 0 {
-		if pipe {
-			// Can't infer from input file name in this mode.
-			fmt.Fprintln(os.Stderr, "[e] No function name specified.")
-			os.Exit(1)
-		}
-
-		_, file := path.Split(*in)
-		file = strings.ToLower(file)
-		file = strings.Replace(file, " ", "_", -1)
-		file = strings.Replace(file, ".", "_", -1)
-		file = strings.Replace(file, "-", "_", -1)
-
-		if unicode.IsDigit(rune(file[0])) {
-			// Identifier can't start with a digit.
-			file = "_" + file
-		}
-
-		fmt.Fprintf(os.Stderr, "[w] No function name specified. Using '%s'.\n", file)
-		*funcname = file
-	}
+	funcName = "E_" + funcName
+	return funcName
 }
